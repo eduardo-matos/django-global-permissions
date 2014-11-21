44db273bf5f98563a187c41ad2edf63642e5656f
Eduardo Matos <eduardo.matos.silva@gmail.com>
2014-11-21 21:04:52 -0200

    lint package with flake8

--- global_permissions/models.py
+++ global_permissions/models.py
@@ -4,10 +4,12 @@ from django.db import models
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
 
+
 class GlobalPermissionManager(models.Manager):
     def get_query_set(self):
-        return super(GlobalPermissionManager, self).\
-                get_query_set().filter(content_type__name='global_permission')
+        return super(GlobalPermissionManager, self).get_query_set().\
+            filter(content_type__name='global_permission')
+
 
 class GlobalPermission(Permission):
     """A global permission, not attached to a model"""
