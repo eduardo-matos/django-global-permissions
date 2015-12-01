5ad90b77bb5b4cc1d67e3c0bff1ae6bd659b5292
Eduardo Matos <eduardo.matos@sieve.com.br>
2015-12-01 10:55:40 -0200

    add required migration for django 1.7+

---
+++ global_permissions/migrations/0001_initial.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('auth', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='GlobalPermission',
+            fields=[
+            ],
+            options={
+                'verbose_name': 'Global Permission',
+                'proxy': True,
+                'verbose_name_plural': 'Global Permissions',
+            },
+            bases=('auth.permission',),
+        ),
+    ]
