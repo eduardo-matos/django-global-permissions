63dd8fdc380cdb5bf91a4f1243c77a1ad3a6a000
FernandoCelmer <email@fernandocelmer.com>
2022-07-09 13:21:48 -0300

    Revert "Task #20 - Fix gettext_lazy and ugettext_lazy import mode"
    This reverts commit a9f6acb72a888833150dde27248362fedd864177.

--- global_permissions/models.py
+++ global_permissions/models.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import platform
 import django
 from django.db import models
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
 
-try:
+if platform.python_version() >= '3.8':
     from django.utils.translation import gettext_lazy as _
-except ImportError:
+else:
     from django.utils.translation import ugettext_lazy as _
 
 
