82e05885f290cc83b8c3210519d83b858a998b0c
FernandoCelmer <email@fernandocelmer.com>
2023-04-28 23:39:07 -0300

    ⚙️ FEATURE: Update Settings

--- setup.py
+++ setup.py
@@ -54,5 +54,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 )
